eda8e6d19cd7b268a3369f99b10b1397f347bda7
Ronald Garcia <ronaldgm@gmail.com>
2026-03-01 14:35:05 -0500

    Add function to create instance of Book and add to array

--- library.js
+++ library.js
@@ -7,4 +7,13 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    bookLibrary.push(newBook);
+}
+
+addBookToLibrary('Sorcerers Stone', 'JKR', 300, true);
+addBookToLibrary('Deathly Hollows', 'JKR', 750, true);
+addBookToLibrary('Cien años de soledad', 'García Márquez', 900, false);
+
 console.table(bookLibrary);
