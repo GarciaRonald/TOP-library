509006d923ba765b2072cf460c7c880451e144b0
Ronald Garcia <ronaldgm@gmail.com>
2026-03-01 17:00:19 -0500

    Add function to display book array on screen

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 const bookLibrary = [];
+const divLibrary = document.getElementById('library');
 
 function Book(title, author, pages, read) {
     this.id = crypto.randomUUID();
@@ -13,8 +14,38 @@ function addBookToLibrary(title, author, pages, read) {
     bookLibrary.push(newBook);
 }
 
+function displayLibrary() {
+    bookLibrary.map(book => {
+        const divBook = document.createElement('div');
+        divBook.id = book.id;
+        divBook.className = 'book';
+
+        const divTitle = document.createElement('div');
+        divTitle.className = 'book-title';
+        divTitle.innerText = book.title;
+
+        const divAuthor = document.createElement('div');
+        divAuthor.className = 'book-author';
+        divAuthor.innerHTML = `by <span class="author">${book.author}</span>`;
+
+        const divPages = document.createElement('div');
+        divPages.className = 'book-pages';
+        divPages.innerText = book.pages ? `${book.pages} pages` : '';
+        
+        const divRead = document.createElement('div');
+        divRead.className = 'book-read';
+        divRead.innerText = `Read: ${book.read ? 'Yes': 'No'}`;
+
+        divLibrary.appendChild(divBook);
+        divBook.appendChild(divTitle);
+        divBook.appendChild(divAuthor);
+        divBook.appendChild(divPages);
+        divBook.appendChild(divRead);
+    });
+}
+
 addBookToLibrary('Sorcerers Stone', 'JKR', 300, true);
 addBookToLibrary('Deathly Hollows', 'JKR', 750, true);
-addBookToLibrary('Cien años de soledad', 'García Márquez', 900, false);
+addBookToLibrary('Cien años de soledad', 'García Márquez', false, false);
 
-console.table(bookLibrary);
+displayLibrary();
